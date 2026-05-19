40afa23ab158df8a2d3c44940ee52380ad8ebeb5
seziitemirbekovna-cmyk <seziitemirbekovna@gmail.com>
2026-05-19 23:31:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,18 @@ st.set_page_config(
 )
 
 st.title("🌐 MT Analyzer")
-st.subheader("Сравнительный анализ машинного перевода")
+st.subheader("Comparative Analysis of Machine Translation Systems")
 
 st.write(
-    "Веб-приложение для анализа перевода интернет-сленга, "
-    "идиоматических выражений и кыргызских культурных конструкций."
+    "This NLP project analyzes the translation of slang, idioms, "
+    "memes, emotional expressions, and Kyrgyz cultural expressions "
+    "using Google Translate, DeepL, and Yandex Translate."
 )
 
 df = pd.read_csv("corpus.csv")
 
 language = st.selectbox(
-    "Выберите язык:",
+    "Select language:",
     ["All"] + list(df["Language"].unique())
 )
 
@@ -27,13 +28,23 @@ if language != "All":
 else:
     filtered_df = df
 
+category = st.selectbox(
+    "Select category:",
+    ["All"] + list(filtered_df["Category"].unique())
+)
+
+if category != "All":
+    filtered_df = filtered_df[filtered_df["Category"] == category]
+
 expression = st.selectbox(
-    "Выберите выражение:",
+    "Choose expression:",
     filtered_df["Expression"]
 )
 
-if st.button("Показать анализ"):
-    row = filtered_df[filtered_df["Expression"] == expression].iloc[0]
+if st.button("Show Analysis"):
+    row = filtered_df[
+        filtered_df["Expression"] == expression
+    ].iloc[0]
 
     st.subheader("Expression")
     st.success(row["Expression"])
@@ -41,8 +52,20 @@ if st.button("Показать анализ"):
     st.subheader("Human Translation")
     st.info(row["Human Translation"])
 
+    st.subheader("Google Translate")
+    st.write(row["Google Translate"])
+
+    st.subheader("DeepL")
+    st.write(row["DeepL"])
+
+    st.subheader("Yandex Translate")
+    st.write(row["Yandex Translate"])
+
     st.subheader("Language")
     st.write(row["Language"])
+
+    st.subheader("Category")
+    st.write(row["Category"])
 
     st.subheader("Comment")
     st.warning(row["Comment"])
@@ -58,6 +81,6 @@ st.dataframe(
 
 st.subheader("Corpus Statistics")
 
-stats = df["Language"].value_counts()
+stats = df["Category"].value_counts()
 
 st.bar_chart(stats)
